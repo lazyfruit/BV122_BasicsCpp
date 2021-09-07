e5ace8c915f32ad7c070c9638b0da0e37d9a49fa
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2021-09-07 22:26:23 +0400

    DynamicMemoryTeaching1

---
+++ Project9DynamicMemory/mainDynamicMemory.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const unsigned int n);
+void Print(int arr[], const unsigned int n);
+
+int* push_back(int arr[], int& n, int value);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	
+	arr = push_back(arr, n, value);
+
+	Print(arr, n);
+	delete[] arr;
+}
+
+void FillRand(int arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void Print(int arr[], const unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+int* push_back(int arr[], int& n, int value)
+{
+	//Добавление элемента в массив:
+	//1. Создаем буферный массив нужного размера:
+	int* buffer = new int[n + 1];
+	//2. Копируем исходный массив в буферный:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3. Удалить исходный массив:
+	delete[] arr;
+	//4. Подменяем исходный массив новым (буферным) массивом, за счет подмены адреса:
+	arr = buffer;
+	//5. Только после всех этих действий можно добавить значение в конец массива:
+	arr[n] = value;
+	//6. После добавления элемента в массив, количество его элементов увеличивается\
+		на 1:
+	n++;
+	return arr;
+}
