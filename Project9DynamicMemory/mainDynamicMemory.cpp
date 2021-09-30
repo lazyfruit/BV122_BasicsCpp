c626eb7766ddc6efeb866db67bda4ca1bb2149e4
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2021-09-30 16:39:26 +0400

    RecursionHomeWorkDone

--- Project9DynamicMemory/mainDynamicMemory.cpp
+++ Project9DynamicMemory/mainDynamicMemory.cpp
@@ -1,39 +1,112 @@
 ﻿#include<iostream>
 using namespace std;
 
-void FillRand(int arr[], const unsigned int n);
-void Print(int arr[], const unsigned int n);
+#define PAUSE system("pause");
 
+//Прототипы функций для одномерных массивов:
+void FillRand(int arr[], const unsigned int n, int minRand = 0, int maxRand = 100);
+void Print(int arr[], const unsigned int n);
 int* push_back(int arr[], int& n, int value);
+int* push_front(int arr[], int& n, int value);
 int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+
+//Прототипы функций для двумерных массивов:
+//void FillRand(int** arr, const unsigned int rows, const unsigned int cols, int minRand = 0, int maxRand = 100);
+//void Print(int** arr, const unsigned int rows, const unsigned int cols);
+//int** allocate(const unsigned int rows, const unsigned int cols);
+//void clear(int** arr, const unsigned int rows);
+//int** push_row_back(int** arr, const unsigned int rows, const unsigned int cols);
+//int** push_row_front(int** arr, const unsigned int rows, const unsigned int cols);
+//void push_col_back(int** arr, const unsigned int rows, unsigned int & cols);
+//int** push_col_front(int** arr, const unsigned int rows, const unsigned int cols);
+
+#define DYNAMIC_MEMORY_1
+//#define DYNAMIC_MEMORY_2
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
 	FillRand(arr, n);
+
+	cout << "\n" << "Полученный массив:" << endl;
 	Print(arr, n);
+	cout << endl;
+	PAUSE;
+
 	int value;
-	cout << "Введите добавляемое значение: "; cin >> value;
-	
+
+	cout << "\n" << "Введите значение которое нужно добавить в конец массива: "; cin >> value;
+	cout << "\n" << "Измененный массив массив:" << endl;
 	arr = push_back(arr, n, value);
 	Print(arr, n);
+	cout << endl;
+	PAUSE;
 
+	cout << "\n" << "Удаляем значение добавленное в конец массива чтобы не мешалось:" << endl;
 	arr = pop_back(arr, n);
 	Print(arr, n);
+
+	cout << "\n" << "Введите значение которое нужно добавить в начало массива: "; cin >> value;
+	cout << "\n" << "Измененный массив массив:" << endl;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+	cout << endl;
+	PAUSE;
+
+	cout << "\n" << "Удаляем значение добавленное в начало массива чтобы не мешалось:" << endl;
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
 	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+	unsigned int rows;
+	unsigned int cols;
+
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строки: "; cin >> cols;
+
+	int** arr = allocate(rows, cols);
+
+	//Обращение к элементам ДДМ:
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n--------------------------------------------------------------\n";
+
+	//arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 200, 300);
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr + 1 << "\t";
+		}
+		cout << endl;
+	}
+
+	push_col_back(arr, rows, cols);
+	for (int i = 0; i < rows; i++) arr[i][cols - 1] = rand();
+	Print(arr, rows, cols);
+
+	clear(arr, rows);
+#endif // DYNAMIC_MEMORY_2
+
 }
 
-void FillRand(int arr[], const unsigned int n)
+void FillRand(int arr[], const unsigned int n, int minRand, int maxRand)
 {
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 100;
+		arr[i] = rand() % (maxRand - minRand) + minRand;
 	}
 }
-
 void Print(int arr[], const unsigned int n)
 {
 	for (int i = 0; i < n; i++)
@@ -42,7 +115,6 @@ void Print(int arr[], const unsigned int n)
 	}
 	cout << endl;
 }
-
 int* push_back(int arr[], int& n, int value)
 {
 	//Добавление элемента в массив:
@@ -53,7 +125,7 @@ int* push_back(int arr[], int& n, int value)
 	{
 		buffer[i] = arr[i];
 	}
-	//3. Удалить исходный массив:
+	//3. Удаляем исходный массив:
 	delete[] arr;
 	//4. Подменяем исходный массив новым (буферным) массивом, за счет подмены адреса:
 	arr = buffer;
@@ -64,11 +136,151 @@ int* push_back(int arr[], int& n, int value)
 	n++;
 	return arr;
 }
+int* push_front(int arr[], int& n, int value)
+{
+	//Добавление элемента в массив:
+	//1. Создаем буферный массив нужного размера:
+	int* buffer = new int[n + 1];
+	//2. Копируем исходный массив в фуферный:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	//3. Удаляем исходный массив:
+	delete[]arr;
+	//4. Подменяем исходнй массив буферным массивом, за счет подмены адреса:
+	arr = buffer;
+	//5. Добавляем значение в начало массива:
+	arr[0] = value;
+	n++;
 
+	return arr;
+}
 int* pop_back(int arr[], int& n)
 {
+	//Удаляем элемент в конце массива:
+	//1. Создаем буферный массив нужного размера:
 	int* buffer = new int[--n];
+	//2. Копируем исходный массив в буферный без последнего элемента:
 	for (int i = 0; i < n; i++) buffer[i] = arr[i];
+	//3. Удаляем исходный массив:
 	delete[]arr;
+
 	return buffer;
 }
+int* pop_front(int arr[], int& n)
+{
+	//Удаляем элемент в конце массива:
+	//1. Создаем буферный массив нужного размера:
+	int* buffer = new int[--n];
+	//2. Копируем исходный массив в буферный без последнего элемента:
+	for (int i = 0; i < n; i++) buffer[i] = arr[i + 1];
+	//3. Удаляем исходный массив:
+	delete[]arr;
+
+	return buffer;
+}
+
+//void FillRand(int** arr, const unsigned int rows, const unsigned int cols, int minRand, int maxRand)
+//{
+//	//		Заполнение ДДМ:
+//	for (int i = 0; i < rows; i++)
+//	{
+//
+//		for (int j = 0; j < cols; j++)
+//		{
+//			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+//		}
+//		cout << endl;
+//	}
+//}
+//void Print(int** arr, const unsigned int rows, const unsigned int cols)
+//{
+//	//		Обращение к элементам ДДМ:
+//	for (int i = 0; i < rows; i++)
+//	{
+//		for (int j = 0; j < cols; j++)
+//		{
+//			cout << arr[i][j] << "\t";
+//		}
+//		cout << endl;
+//	}
+//}
+//int** allocate(const unsigned int rows, const unsigned int cols)
+//{
+//	//		Объявление динамического массива:
+//	//1. Объявляем указатель на указатель и сохраняем в него адрес массива указателей:
+//	int** arr = new int* [rows];
+//	//2. Создаем строки ДДМ:
+//	for (int i = 0; i < rows; i++)
+//	{
+//		arr[i] = new int[cols] {};
+//	}
+//	return arr;
+//}
+//void clear(int** arr, const unsigned int rows)
+//{
+//	//		Удаление ДДМ:
+////1. Удаляем строки:
+//	for (int i = 0; i < rows; i++)
+//	{
+//		delete[]arr[i];
+//	}
+//	//2. Удаляем массив указателей:
+//	delete[]arr;
+//}
+//int** push_row_back(int** arr, unsigned int& rows, const unsigned int cols)
+//{
+//	//1)Создаём буфферный массив указателей:
+//	int** buffer = new int* [rows + 1]{};
+//	//2)Копируем адреса строк в буфферный массив указателей
+//	for (int i = 0; i < rows; i++)
+//	{
+//		buffer[i] = arr[i];
+//	}
+//	//3)Удаляем исходный массив указателей:
+//	delete[] arr;
+//	//4)Подменяем исходный массив буфферным:
+//	arr = buffer;
+//	//5)Добавляем в массив новую строку:
+//	arr[rows] = new int[cols] {};
+//	//6)Увеличиваем количество строк:
+//	rows++;
+//	//7)Возвращаем новый массив на место вызова:
+//	return arr;
+//}
+//int** push_row_front(int** arr, unsigned int& rows, const unsigned int cols)
+//{
+//	int** buffer = new int* [rows + 1]{};
+//	for (int i = 0; i < rows; i++)
+//	{
+//		buffer[i + 1] = arr[i];
+//	}
+//	delete[] arr;
+//	arr = buffer;
+//	arr[0] = new int[cols] {};
+//	rows++;
+//	return arr;
+//}void push_col_back(int** arr, const unsigned int rows, unsigned int & cols)
+//{
+//	for (int i = 0; i < rows; i++)
+//	{
+//		//1. Создаем буферную строку, на 1 элемент больше:
+//		int* buffer = new int[cols + 1]{};
+//		//2. Копируем исходную строку в буфферную:
+//		for (int j = 0; j < cols; j++)
+//		{
+//			buffer[j] = arr[i][j];
+//		}
+//		//3. Удаляем исходную строку:
+//		delete[] arr[i];
+//		//4. Присваиваем исходной строке и адрес буферной строки:
+//		arr[i] = buffer;
+//	}
+//	//5. После того, как в каждой строке добавилосб по элементу, колличество столбцов увеличилось на 1:
+//	cols++;
+//}
+//int** push_col_front(int** arr, const unsigned int rows, const unsigned int cols)
+//{
+//
+//}
--- ProjectzRecursion/mainRecursion.cpp
+++ ProjectzRecursion/mainRecursion.cpp
@@ -1,26 +1,89 @@
 ﻿#include<iostream>
 using namespace std;
 
-void elevator(int floor)
-{
-	if (floor == 0) //Условие выхода из рекурсии
-	{
-		//cout << "Вы в подвале" << endl;
-		cout << floor << endl;
-		return;
-	}
-	cout << floor << endl;
-	elevator(floor-1);
-	cout << floor << endl;
-}
+//#define ELEVATOR
+#define FACTORIAL
+//#define POWER
+//#define FIBONACCI
+
+void Elevator(int floor);
+int Factorial(int n);
+double Power(int a, int n);
+void Fibonacci(int n/*, int a, int b*/);
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	/*cout << "HelloWorld";
-	main();*/
 
+#ifdef ELEVATOR
 	int floor;
 	cout << "Введите номер этажа: "; cin >> floor;
-	elevator(floor);
+	Elevator(floor);
+#endif // ELEVATOR
+
+#ifdef FACTORIAL
+	int n;
+	cout << "Введите число: "; cin >> n;
+	cout << "\n" << "Факториал числа: " << Factorial(n) << endl;
+#endif // FACTORIAL
+
+#ifdef POWER
+	int a;
+	int n;
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	cout << "\n" << "Полученное значение степени: " << Power(a, n) << endl;
+#endif // POWER
+
+#ifdef FIBONACCI
+	int n;
+	cout << "До какого предела вывести ряд Фибоначчи? "; cin >> n;
+	cout << "\n" << "Полученный ряд Фибаначи в пределе до числа " << n << " :" << endl;
+	Fibonacci(n);
+	cout << endl;
+#endif // FIBONACCI
+}
+
+void Elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Вы в подвале" << endl;
+		return;
+	}
+	cout << floor << endl;
+	Elevator(floor - 1);
+	cout << floor << endl;
+}
+
+int Factorial(int n)
+{
+	if (n == 0)return 1;
+	int f = n * Factorial(n - 1);
+	return f;
+}
+
+double Power(int a, int n)
+{
+	if (n == 0) return 1;
+	if (n < 0)
+	{
+		return 1. / a * Power(a, n + 1);
+	}
+	return a * Power(a, n - 1);
+	//Вариант записи №2
+	//return n == 0 ? 1 : n > 0 ? a * Power(a, n - 1) : 1. / a * Power(a, n + 1);
+}
+
+void Fibonacci(int n/*, int a, int b*/)
+{
+	static int a = 0, b = 1, c = 1;
+	if (a > n)return;
+	cout << a << "\t";
+	//Fibonacci(n, b, a + b);
+	/*a = b;
+	b = c;
+	c = a + b;*/
+	c = (a = b) + (b = c);
+	Fibonacci(n);
 }
