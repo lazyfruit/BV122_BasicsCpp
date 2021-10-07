1ea60bab815fc95b5e4cd0a41abf15f952beeade
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2021-10-07 22:29:46 +0400

    Строки

---
+++ Projecta10NullTerminatedLines/mainNullTerminatedLines.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<Windows.h>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int StringLength(char str[]);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	//char str[] = { 'H', 'e', 'l','l','o', 0 };
+	/*char str[] = "Hello";*/
+	const int n = 20;
+	char str[n]{};
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите строку: ";
+	cin.getline(str, n);
+	cout << str << endl;
+	StringLength(///);
+}
+
+int StringLength(char str[])
+{
+	cout << sizeof(///);
+}
