a1fe7d1abc6f6ce9568ab2f155d80e5227b9b862
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2022-01-31 13:54:41 +0400

    Commit

--- Project1Hello/main1Hello.cpp
+++ Project1Hello/main1Hello.cpp
@@ -1,17 +1,19 @@
-﻿#include<iostream> //cin/cout
-// include - подключить/включить в состав/подсоединить
+﻿//#include<iostream>//Библиотека содержащая cin/cout
+//include (подключить/включить в состав/подсоединить) - директива препроцессора
 using namespace std;
 
-//Глобальное пространсво имен (Global scope)
+//Глобальное пространсво имен (global scope)
 
 void main()
 {
-	setlocale(LC_ALL, "Russian");//Устанавливаем пусскую кодировку.\
-								 Возможно писать Rus или оставлять авычки пустыми.
-	/*cout << "\n\tHello\"World\"\n";
+	setlocale(LC_ALL, "Russian");//Устанавливаем русскую кодировку.\
+								 Возможно писать Rus или оставлять кавычки пустыми.
+	/*cout << "HelloWorld"*/
+	cout << "\n\tHello\"World\"\n";
 	cout << "\nПриветМир\n";
-	cout << "\nС:\\Windows\\System32\\drivers\\\n";*/
+	cout << "\nС:\\Windows\\System32\\drivers\\\n";
 }
+// Ctrl + f7 - Компиляция
+// Ctrl + f5 – Запуск
 
-// ctrl + f7 - Компиляция
-// ctrl + f5 – Запуск
+//Глобальное пространсво имен (global scope)
